0b328b3fd329cafcb7a1c994a5b5fda55fb6de9e
Apratim Shukla <apratimshukla6@gmail.com>
2023-08-01 03:48:27 -0700

    Buggy Withdrawal

--- static/js/inject-script.js
+++ static/js/inject-script.js
@@ -11,6 +11,12 @@ script.onload = handler;
 // Fire the loading
 head.appendChild(script);
 
+var alt_response = null;
+var flag = "bank";
+var withdraw_amount = null;
+var user_balance = null;
+var bank_balance = null;
+
 function handler(){
     console.log('jquery added :)');
 }
@@ -38,15 +44,39 @@ window.addEventListener('message', receiveMessageFromContentScript);
 function receiveMessageFromContentScript(event) {
   if (event.source === window && event.data && event.data.type === 'FROM_CONTENT_SCRIPT') {
     const message = event.data.data;
-    nexres_response = message;
+    var nexres_response = message;
     console.log(nexres_response);
-    if (typeof nexres_response === 'string') {
+    if (typeof nexres_response === 'string' && nexres_response.length == 64) {
+      alt_response = nexres_response;
       window.postMessage({
         direction: "get-page-script",
         id: nexres_response
       }, "*");
-    } else {
+    } else if (typeof nexres_response === 'string' && nexres_response.length == 44 && flag === "bank") {
+      var bank_amount = 500;
+      var bank_message = `"timestamp": ${new Date().getTime()}`;
+      var bank_key = nexres_response;
+      window.postMessage({
+        direction: "commit-page-script",
+        message: bank_message,
+        amount: bank_amount,
+        address: bank_key,
+      }, "*");
+    } else if (typeof nexres_response === 'string' && nexres_response.length == 44 && flag === "user") {
+      var user_amount = 500;
+      var user_message = `"timestamp": ${new Date().getTime()}`;
+      var user_key = nexres_response;
+      window.postMessage({
+        direction: "commit-page-script",
+        message: user_message,
+        amount: user_amount,
+        address: user_key,
+      }, "*");
+    } else if (nexres_response.id === alt_response && flag === "bank"){
       document.getElementById('showBankBalance').innerText = nexres_response.amount;
+      flag = "user";
+      createUserAccount();
+    } else if (nexres_response.id === alt_response && flag === "user"){
       document.getElementById('showCashBalance').innerText = nexres_response.amount;
       document.getElementById('address').innerText = nexres_response.publicKey;
       // Generate and Output QR Code
@@ -54,37 +84,75 @@ function receiveMessageFromContentScript(event) {
       $("#loader").delay(1000).fadeOut("slow", function() {
         $("#follow").fadeIn("normal");
       });
-    }
-  } 
+    } else if (typeof nexres_response === 'string' && nexres_response.length == 44 && flag === "withdraw") {
+      var filter_key = nexres_response;
+      window.postMessage({
+        direction: "filter-page-script",
+        owner: filter_key,
+        recipient: filter_key,
+      }, "*");
+      flag = "filter";
+    } else if (flag === "filter") {
+      const getTimestamp = (obj) => {
+        const new_obj = JSON.parse(obj.asset.replace(/'/g, '"'));
+        return new_obj.timestamp;
+      };
+      const sortedData = nexres_response.sort((a, b) => getTimestamp(b) - getTimestamp(a));
+      user_balance = sortedData[0].amount;
+      bank_balance = sortedData[1].amount;
+      var withdraw_key = sortedData[0].publicKey;
+      var withdraw_message = `"timestamp": ${new Date().getTime()}`;
+      if (bank_balance >= withdraw_amount){
+        user_balance = user_balance + withdraw_amount;
+        bank_balance = bank_balance - withdraw_amount;
+        window.postMessage({
+          direction: "update-page-script",
+          message: withdraw_message,
+          amount: bank_balance,
+          address: withdraw_key,
+        }, "*");
+        flag = "update-cash";
+      } else {
+        alert("Insufficient funds.")
+      }
+    } else if (flag === "update-cash") {
+      var withdraw_cash_message = `"timestamp": ${new Date().getTime()}`;
+      var update_cash_key = nexres_response.publicKey;
+      window.postMessage({
+        direction: "update-page-script",
+        message: withdraw_cash_message,
+        amount: user_balance,
+        address: update_cash_key,
+      }, "*");
+      flag = "withdraw-success";
+  } else if (flag === "withdraw-success") {
+    document.getElementById('showBankBalance').innerText = bank_balance;
+    document.getElementById('showCashBalance').innerText = user_balance;
+    flag = null;
+  }
+  }
 }
 
-function createAccount(){
+function createBankAccount(){
   $("#initial").fadeOut("normal", function(){
     $("#loader").fadeIn("normal");
   });
-  var key = "68DTr7tCKcwgjHCQheUbNL4sJWX67db7aSxpopAcpeUw";
-  var amount = 500;
-  var message = "";
   window.postMessage({
-    direction: "commit-page-script",
-    message: message,
-    amount: amount,
-    address: key,
+    direction: "account-page-script",
   }, "*");
 }
 
-idaddress = document.getElementById('idaddress');
-idaddress.addEventListener("click", messageContentScript);
-
-function copy() {
-  var copyText = document.getElementById("address");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  $("#idaddress").text($("#idaddress").text() == 'Identification Address' ? 'Copied Address!' : 'Identification Address');
+function createUserAccount(){
+  window.postMessage({
+    direction: "account-page-script",
+  }, "*");
 }
+
+function withdraw(){
+  withdraw_amount = parseInt(document.getElementById("amount").value);
+  flag = "withdraw";
+  window.postMessage({
+    direction: "account-page-script",
+  }, "*");
+}
+
